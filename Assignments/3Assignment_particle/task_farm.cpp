1f9259b454f359dcc49d68039e24644c5847a3eb
2BeAsh <109065992+2BeAsh@users.noreply.github.com>
2024-02-24 10:36:09 +0100

    Update task_farm.cpp
    Does not work on SLURM, works otherwise

--- Assignments/3Assignment_particle/task_farm.cpp
+++ Assignments/3Assignment_particle/task_farm.cpp
@@ -10,6 +10,7 @@
 #include <chrono>
 #include <thread>
 #include <array>
+#include <vector>
 
 // To run an MPI program we always need to include the MPI headers
 #include <mpi.h>
@@ -17,45 +18,60 @@
 const int NTASKS=5000;  // number of tasks
 const int RANDOM_SEED=1234;
 
+
 void master (int nworker) {
     std::array<int, NTASKS> task, result;
 
     // set up a random number generator
     std::random_device rd;
-    //std::default_random_engine engine(rd());
     std::default_random_engine engine;
     engine.seed(RANDOM_SEED);
-    // make a distribution of random integers in the interval [0:30]
     std::uniform_int_distribution<int> distribution(0, 30);
 
     for (int& t : task) {
         t = distribution(engine);   // set up some "tasks"
     }
 
-    /*
-    IMPLEMENT HERE THE CODE FOR THE MASTER
-    ARRAY task contains tasks to be done. Send one element at a time to workers
-    ARRAY result should at completion contain the ranks of the workers that did
-    the corresponding tasks
-    */
-    
-    // Loop through tasks and send them to a random worker
-    // Receive rank from worker
-    for (int i=0; i<NTASKS; i++){
-        // Pick random rank/worker to send task to
-        std::uniform_int_distribution<int> distribution_worker(1, nworker+1);
-        int worker_rank = distribution_worker(engine);
-        int task_i = task[i];
-        int rank;
-        MPI_Send(&task_i, 1, MPI_INT, worker_rank, 2, MPI_COMM_WORLD);  // Send task
-        MPI_Recv(&rank, 1, MPI_INT, worker_rank, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);  // Receive rank from worker
-        result[i] = rank;
+    int number_of_tasks_sent = 0;
+    // 1. Give all workers a job
+    for (int i = 1; i <= nworker; i++) {
+        MPI_Send(&task[number_of_tasks_sent], 1, MPI_INT, i, number_of_tasks_sent, MPI_COMM_WORLD);  // Can be non-blocking. Request for each individual Send
+        number_of_tasks_sent++;
+    }
+
+    // 2. While there are jobs left, 
+    int current_result;
+    int result_index;
+    int worker_source;
+    int send_tag;
+    MPI_Status status;
+    while (number_of_tasks_sent < NTASKS) {
+        // Receive data and get worker rank and result index (i.e. time sent)
+        MPI_Recv(&current_result, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+        worker_source = status.MPI_SOURCE;
+        result_index = status.MPI_TAG;
+        // Update result 
+        result[result_index] = current_result;
+        // Send message
+        send_tag = number_of_tasks_sent;
+        MPI_Send(&task[number_of_tasks_sent], 1, MPI_INT, worker_source, send_tag, MPI_COMM_WORLD);
+        number_of_tasks_sent++;
+    }
+
+    int shutdown_tag = NTASKS + 10;
+    for (int j = 1; j <= nworker; j++) {
+        // Get and store result
+        MPI_Recv(&current_result, 1, MPI_INT, j, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+        int result_index = status.MPI_TAG;
+        result[result_index] = current_result;
+        // Send shutdown 
+        MPI_Send(&result[0], 1, MPI_INT, j, shutdown_tag, MPI_COMM_WORLD);
     }
 
     // Print out a status on how many tasks were completed by each worker
-    for (int worker=1; worker<=nworker; worker++) {  // Loop over workers
+    for (int worker=1; worker<=nworker; worker++) {
         int tasksdone = 0; int workdone = 0;
-        for (int itask=0; itask<NTASKS; itask++)  // Loop over tasks
+        for (int itask=0; itask<NTASKS; itask++)
         if (result[itask]==worker) {
             tasksdone++;
             workdone += task[itask];
@@ -70,19 +86,26 @@ void task_function(int task) {
     std::this_thread::sleep_for(std::chrono::milliseconds(task));
 }
 
+
 void worker (int rank) {
-    /*
-    IMPLEMENT HERE THE CODE FOR THE WORKER
-    Use a call to "task_function" to complete a task
-    */
-   int task_i;
-   MPI_Recv(&task_i, 1, MPI_INT, 0, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);  // Receive task from Master
-   task_function(task_i);  // Perform task
-   MPI_Send(&rank, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);  // send rank to Master
+    // Get initial task to have a task to check if "received_task != -1" is true.
+    int received_task;
+    MPI_Status status;
+    int master_rank = 0;
+    MPI_Recv(&received_task, 1, MPI_INT, master_rank, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+    int received_tag = status.MPI_TAG;
+
+    while (received_tag != NTASKS + 10) {  // NTASKS + 10 is shutdown tag
+        task_function(received_task);
+        MPI_Send(&rank, 1, MPI_INT, master_rank, received_tag, MPI_COMM_WORLD);
+        
+        MPI_Recv(&received_task, 1, MPI_INT, master_rank, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+        received_tag = status.MPI_TAG;
+    }
 }
 
 int main(int argc, char *argv[]) {
-    int nrank, rank;  //nrank = nproces i.e. number of workers plus the master worker. rank is current worker
+    int nrank, rank;
 
     MPI_Init(&argc, &argv);                // set up MPI
     MPI_Comm_size(MPI_COMM_WORLD, &nrank); // get the total number of ranks
